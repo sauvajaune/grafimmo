0175423fc2a02ea9136cb42c80f8472f40005a8a
sauvajaune <sauvagementjaune@gmail.com>
2019-02-01 15:56:20 +0100

    formualaire inscripiton fin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -32,10 +32,14 @@ class SecurityController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
             $hash = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hash);
+
             $manager->persist($user);
             $manager->flush();
+
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('security/inscription.html.twig', [
