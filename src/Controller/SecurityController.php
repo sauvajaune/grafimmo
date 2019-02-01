58caaf83e7eba77e1c63fa287cffb0496e378629
sauvajaune <sauvagementjaune@gmail.com>
2019-02-01 15:28:09 +0100

    formualaire inscripiton

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,13 +8,42 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        $form=$this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return $this->render('security/inscription.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
 
     /**
      * @Route("/login", name="login")
